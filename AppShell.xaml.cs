645f99218109b45272f34d56db52fdd21d8194ba
hcraft <36808098+dakto101@users.noreply.github.com>
2022-12-08 08:48:52 +0700

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using IE307.N11.ViewModels;
+using IE307.N11.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace IE307.N11
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IE307.N11.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
